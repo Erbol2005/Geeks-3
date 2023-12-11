95b8ce71ee3ba2223d151ff1a599e382c69aee75
Erbol2005 <ebokator@gmail.com>
2023-12-11 23:46:38 +0600

    'HW1

---
+++ HW-1_36-3.py
@@ -0,0 +1,58 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+from os import getenv
+from aiogram.filters import Command
+import logging
+import os
+import random
+
+
+load_dotenv()
+bot = Bot(token=getenv('BOT_TOKEN'))
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.full_name}")
+
+
+@dp.message(Command("random_pic"))
+async def send_random_pic(message: types.Message):
+    images_list = ['1.webp', '2.webp', '3.webp', '4.webp', 'i.webp']
+    images_dir = "images/"
+
+    random_image = random.choice(images_list)
+    image_path = os.path.join(images_dir, random_image)
+
+    file = types.FSInputFile(image_path)
+    await message.answer_photo(
+        photo=file,
+    )
+
+
+@dp.message(Command("my_info"))
+async def my_info(message: types.Message):
+    await message.reply(
+        f'''
+        Ваш ID: {message.from_user.id}\n 
+        Ваше имя: {message.from_user.first_name}\n
+        Ваше пользовательское имя: {message.from_user.username}\n
+        '''
+    )
+
+
+async def main():
+    await bot.set_my_commands([
+        types.BotCommand(command="start", description="Начало"),
+        types.BotCommand(command="my_info", description="Моя информация"),
+        types.BotCommand(command="random_pic", description="Случайная картинка")
+    ])
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
--- lessen-3_1.py
+++ lessen-3_1.py
@@ -1,11 +1,12 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from dotenv import load_dotenv
 from os import getenv
 import logging
 
-BOT_TOKEN = "6882174935:AAFBW-hD9Smw5vLGWk3lnxY4UIZtVCYQPc0"
-bot = Bot(token=BOT_TOKEN)
+load_dotenv()
+bot = Bot(token=getenv('BOT_TOKEN'))
 dp = Dispatcher()
 
 
@@ -17,11 +18,26 @@ async def send_pic(messange: types.Message):
         caption="Котик"
     )
 
+@dp.message(Command=['myinfo'])
+async def my_info(message: types.Message):
+    user_info = (
+        f"Ваш ID: {message.from_user.id}\n"
+        f"Ваше имя: {message.from_user.first_name}\n"
+        f"Ваш никнейм: {message.from_user.username}"
+    )
+    await message.answer(user_info)
+
 @dp.message()
 async def echo(message: types.Message):
     await message.answer(f"{message.text}, {message.from_user.first_name}")
 
 async def main():
+    await bot.set_my_commands([
+        types.BotCommand(command="start", description="Начало"),
+        types.BotCommand(command="pic", description="Получить картинку")
+    ])
+
+    # запускаем бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
